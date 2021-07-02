535f05324d3fccaf15c2d7c507dc51bef2d67321
wzhzzmzzy <wzhzzmzzy@gmail.com>
2021-07-02 18:45:54 +0800

    feat: 添加一个切换主题的affix

--- src/layout/default/index.vue
+++ src/layout/default/index.vue
@@ -1,27 +1,42 @@
-<template>
-  <n-config-provider :theme="theme">
-    <slot/>
-  </n-config-provider>
+<template lang="pug">
+  n-config-provider(:theme="theme")
+    slot
+    n-config-provider(:theme="reverseTheme" namespace="reverse")
+      n-affix.switch-theme-affix(:bottom="120" :trigger-bottom="60")
+        n-button.switch-theme-affix__btn(circle :size="large" type="primary" @click="switchTheme")
+          template(#icon)
+            n-icon
+              moon-stars(v-if="currentTheme === 'light'")
+              sun(v-else)
 </template>
 
 <script lang="ts">
-import {defineComponent, ref, watch} from "vue";
-import {NConfigProvider, darkTheme, useOsTheme} from 'naive-ui';
+import {defineComponent, ref, toRefs, watch} from "vue";
+import {NConfigProvider, NAffix, NButton, NIcon, darkTheme, useOsTheme} from 'naive-ui';
+import {MoonStars, Sun} from '@vicons/carbon';
 import {useGlobalStore} from "../../store/global";
 
 export default defineComponent({
   name: 'DefaultLayout',
   components: {
-    NConfigProvider
+    NConfigProvider,
+    NAffix,
+    NButton,
+    NIcon,
+    MoonStars,
+    Sun
   },
   setup() {
     const globalStore = useGlobalStore();
     const osThemeRef = useOsTheme();
     const theme = ref<any>(null);
+    const reverseTheme = ref<any>(darkTheme);
+    const {currentTheme} = toRefs(globalStore);
     watch(() => ({
       currentTheme: globalStore.currentTheme,
     }), ({currentTheme}) => {
       theme.value = currentTheme === 'dark' ? darkTheme : null;
+      reverseTheme.value = currentTheme === 'dark' ? null : darkTheme;
     });
     if (osThemeRef.value === 'dark') {
       globalStore.$patch({
@@ -29,8 +44,25 @@ export default defineComponent({
       })
     }
     return {
-      theme
+      theme,
+      reverseTheme,
+      currentTheme,
+      switchTheme() {
+        globalStore.$patch({
+          currentTheme: currentTheme.value === 'light' ? 'dark' : 'light'
+        })
+      }
     }
   }
 });
 </script>
+
+<style lang="scss" scoped>
+.switch-theme-affix {
+  right: 60px;
+
+  &__btn {
+    font-size: 30px;
+  }
+}
+</style>
